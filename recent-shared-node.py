ce52f1d3e8c5ff57c56cba29a6605c280d27ba1e
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-06-08 09:14:25 -0600

    Create a TimeTree class to automatically assign time values

--- recent-shared-node.py
+++ recent-shared-node.py
@@ -2,6 +2,39 @@
 
 from ete3 import Tree, NodeStyle, TreeStyle
 
+class TimeTree(Tree):
+	"""
+	TimeTree is an ete3 Tree with timestamps associated with the branch
+	lengths. The most recent node is at time 0, and those further back in the tree
+	have positive time values so that they can be used in coalescent modeling.
+
+	most_recent_mixed_node()
+		Return the most recent node in a tree (the lowest time value) that leads
+		to more than one type of descendant, as decided by name.
+	
+	apply_time_values()
+		Based on the branch lengths in the tree, assign time values starting
+		from zero at the furthest leaf and increasing backwards in the tree.
+		Automatically called on __init__.
+	"""
+
+	def __init__(self, *args, **kwargs):
+		super(TimeTree, self).__init__(*args, **kwargs)
+		self.apply_time_values()
+		
+	def most_recent_mixed_node(self):
+		best = self
+		for node in get_mixed_nodes(self):
+			if node.time < best.time:
+				best = node
+		return best
+	
+	def apply_time_values(self):
+		tree_max = self.get_farthest_node()[1]
+		for node in self.traverse():
+			node_dist = distance_from_root(node)
+			node.time = tree_max - node_dist
+			
 # Create a list of all nodes in a tree that satisfy is_mixed_node.
 def get_mixed_nodes(tree):
 	mixed = []
@@ -20,22 +53,12 @@ def distance_from_root(node):
 
 # Return the mixed node with the largest distance from
 # the root of the tree
-# TODO: This may actually not be working. Test with more trees ASAP.
 def most_recent_mixed_node(tree):
 	best = tree
 	for node in get_mixed_nodes(tree):
 		if node.time < best.time:
 			best = node
 	return best
-
-# Add a time parameter to each node in the tree reflecting its distance from
-# the tips of the tree. The most recent node will have a time of 0, and the
-# oldest nodes will have large positive times.
-def apply_time_values(tree):
-	tree_max = tree.get_farthest_node()[1]
-	for node in tree.traverse():
-		node_dist = distance_from_root(node)
-		node.time = tree_max - node_dist
 
 # TODO: How can this work?
 # It should put a node (possibly with some title) at every branch of the tree
@@ -57,9 +80,8 @@ def insert_node_at_time(tree, time): #TODO: Does it need to be a tree, or is it 
 		for child in tree.children:
 			insert_node_at_time(child, time)
 
-def get_example_tree():
-	t = Tree("challenge.nwk")
-	apply_time_values(t)
+def get_example_tree(filename):
+	t = TimeTree(filename)
 
 	point = NodeStyle()
 	point["fgcolor"] = "lightgreen"
@@ -73,7 +95,6 @@ def get_example_tree():
 	return t, ts
 
 if __name__ == "__main__":
-	t, ts = get_example_tree()
-	# t.render("it_not_work.png", w=400, tree_style=ts)
+	t, ts = get_example_tree("challenge.nwk")
 	t.show(tree_style=ts)
 

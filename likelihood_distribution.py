9506a91e5fbbe189220b70a9d4c16c4522b94596
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-06-13 17:00:50 -0600

    Add binomial coefficient, make display use non-log likelihood

--- likelihood_distribution.py
+++ likelihood_distribution.py
@@ -2,6 +2,7 @@
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.stats as stats
 import sys # at the moment I use it for sys.argv but maybe I don't need that idk
 import math
 
@@ -18,12 +19,26 @@ def count_faces(string):
 			tails += 1
 	return heads, tails
 
+def binomial_coeff(n, x):
+	"""
+	Calculate a binomial coefficient based on the number of heads (x) in 
+	n flips of the coin, as described by Etz (2018).
+		https://doi.org/10.1177/2515245917744314
+	"""
+	return math.factorial(n) / (math.factorial(x) * math.factorial(n - x))
+
+def likelihood(p, heads, tails):
+	"""
+	Calculate the likelihood of a certain p (0 to 1, exclusice) based on observed
+	numbers of heads and tails
+	"""
+	return pow(p, heads) * pow(1-p, tails)
+
 def log_likelihood(p, heads, tails):
 	"""
 	Calculate the log likelihood of a certain p (0 to 1, exclusive) based on observed
 	numbers of heads and tails
 	"""
-	#print("p", p, "heads", heads, "tails", tails)
 	return math.log(pow(p, heads)) + math.log(pow(1-p, tails))
 
 def likelihood_curve(heads, tails):
@@ -31,19 +46,20 @@ def likelihood_curve(heads, tails):
 	Given a number of heads and tails, construct a curve showing
 	the log likelihood of p between 0 and 1.
 	"""
-	x = np.linspace(0, 1, 1000, endpoint=False)[1:] #TODO: fix this messy way to exclude both 0 and 1
+	x = np.linspace(0, 1, 1000, endpoint=False)[1:] #TODO: Can I make this less ugly? 
 	y = []
+	bc = binomial_coeff(heads + tails, heads)
 	for p in x:
-		y.append(log_likelihood(p, heads, tails))
+		y.append(bc * likelihood(p, heads, tails)) 
 	return x, y
 
-def important_points(x, y): #TODO: This doesn't actually return anything useful at the moment.
+def important_points(x, y): 
 	"""
 	Given a likelihood curve, find the maximum point on the curve, 
 	as well as the positions that make up a 95% confidence interval.
 	"""
-	maximum = 1
-	lower = 0.1
+	maximum = y.index(max(y))
+	lower = 0.1 # TODO: Find the right way to calculate these figures. Do not assume binomial dist!
 	upper = 0.9
 	return maximum, lower, upper
 
@@ -52,8 +68,7 @@ if __name__ == "__main__":
 	x, y = likelihood_curve(h, t)
 
 	max_pos = y.index(max(y))
-
-	print(x[max_pos], y[max_pos])
+	print("max of", y[max_pos], "at x =", x[max_pos])
 
 	plt.plot(x, y)
 	plt.show()

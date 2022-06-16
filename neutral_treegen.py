fd14b5b99d39d5583be7ef3a6f30f4bf4efdec22
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-06-16 13:23:04 -0600

    One attempt at plotting the convergence time, but it didn't really work

--- neutral_treegen.py
+++ neutral_treegen.py
@@ -12,8 +12,10 @@ It seems to be a part of a university publication, but that's all I have.
 """
 
 import random
-from ete3 import TreeNode
 import matplotlib.pyplot as plt
+import math
+import numpy as np
+from ete3 import TreeNode
 
 def create_nodes(N):
 	nodes = []
@@ -30,6 +32,7 @@ def step(nodes, N):
 			parents.append(parent)
 		else:
 			parent = [p for p in parents if p.name == parent_name][0]
+			print("just ch se the same parent as another node", parent_name)
 		parent.add_child(node)
 	return parents
 
@@ -46,18 +49,36 @@ def generate_tree(N):
 		lineages.append(len(nodes))
 	return nodes[0], times, lineages
 
-def likelihood_curve(N):
-	# what on earth should the curve be??!?!?!?!?!
-	# new idea: start with a bunch of lineages and plot how many there are at
-	# different times and maybe that will be something that's even vaguely useful
-	pass
-
 def plot_lineages(N):
+	"""
+	Run the generation model and plot how many lineages exist at each timestep.
+	"""
 	tree, x, y = generate_tree(N)
 	plt.plot(x, y)
 	plt.show()
-	print(times)
-	print(lineages)
+
+def prob_all_lineages_converge(k, N, t):
+	"""
+	Given k lineages out of N initial lineages, return the probability
+	that all the lineages combine at time t and no earlier.
+	This is all I can think to do properly. If they could combine in multiple stages,
+	everything opens up way past any understanding I have of the situation.
+	"""
+	# single generation where they coalesce
+	success_generation = 1 / math.pow(N, k-1)
+	# t-1 generations whete they do not coalesce
+	failed_generations = math.pow(1 - success_generation, t-1)
+
+	# multiplied, they give the overall probability of that t
+	return failed_generations * success_generation 
+
+def plot_convergence_time(k, N):
+	x = np.linspace(0, N)
+	y = []
+	for t in x:
+		y.append(prob_all_lineages_converge(k, N, t))
+	plt.plot(x, y)
+	plt.show()
 
 if __name__ == "__main__":
 	plot_lineages(100)

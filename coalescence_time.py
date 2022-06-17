210281a41382941988fdb10db73ea2cf512ce2c5
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-06-17 09:08:47 -0600

    Created coalescent treegen that reports the time until coalescence

---
+++ coalescence_time.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def time_until_coalescence(N, k):
+    """
+    Run a neutral coalescent model until a coalescence occurs,
+    returning the total number of generations.
+    """
+    nodes = range(k) # ensure that all are unique in the first step (time 1)
+    time = 0
+    while len(nodes) == k: # TODO: Do I want >= k to be safe or is it fine?
+        time += 2
+        nodes = set([random.randint(1, N-1) for i in nodes])
+    return time
+
+def batch_sim(N, k, iterations):
+    """
+    Run many coalescence simulations and concatenate the time that each
+    simulation took.
+    """
+    return [time_until_coalescence(N, k) for i in range(iterations)]
+
+def plot_coalescence_time(N, k, iterations):
+    """
+    Plot a histogram
+    """
+    times = batch_sim(N, k, iterations)
+    plt.hist(times)
+    plt.show()
+
+if __name__ == "__main__":
+    plot_coalescence_time(10, 3, 100)

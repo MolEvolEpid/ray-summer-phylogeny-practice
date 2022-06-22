34e0336fbd6b5d2a1f4c03056b25c4860ee9e024
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-06-22 15:39:17 -0600

    Add log likelihood of coalescence

--- tree_likelihood.py
+++ tree_likelihood.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 
-from population_models import con_probability, lin_probability, exp_probability
-from time_tree import TimeTree 
+import numpy as np
+from population_models import con_probability #, lin_probability, exp_probability
+from time_tree import TimeTree
 
-t = TimeTree("((A:1, B:1):2, C:3);")
-
-def closest_parent_node(time, tree):
+def closest_parent_node(tree, time):
     """
     Return the next closest node backwards from the specified time.
     (for instance, the next node inwards from the leaves at time 0)
     """
-    closest_node = tree # the root has the highest time so will always be replaced if possible
+    closest_node = tree
     for node in tree.traverse():
         # Any candidates need to be:
         #     Sooner than the current best time
@@ -20,5 +19,52 @@ def closest_parent_node(time, tree):
             closest_node = node
     return closest_node
 
-# okay, what needs to happen?
-# find the k and n and t of a certain bit of the tree (and assume for now it's constant pop)
+def children_at_time(node, time):
+    """
+    Return the number of a node's children that exist at a certain time.
+    """
+    if time >= node.time:
+        raise Exception("Time must be after the node's own time")
+    return [n for n in node.traverse() if n.time <= time < n.up.time]
+
+def tree_segments(tree):
+    """
+    Divide the tree into a list of segments based on where coalescences occur.
+    """
+    max_time = tree.time
+    segments = []
+    time = 0
+    while time < max_time:
+        next_parent = closest_parent_node(tree, time)
+        segments.append((time, next_parent.time))
+        time = next_parent.time
+    return segments
+
+def segment_log_likelihood(tree, start, end):
+    """
+    Return the log likelihood that a certain segment of the tree would coalesce the way
+    it did, assuming an arbitrary population size of 1000.
+    """
+    k = len(children_at_time(tree, start))
+    # TODO: when there are multiple start times this will be innaccurate
+    N = 1000
+    z = end - start
+    return np.log(con_probability(k, N, z))
+
+def tree_log_likelihood(tree):
+    """
+    Return the log likelihood that a certain tree exists (normal likelihood 
+    may underflow to zero, which we don't want)
+    """
+    segments = tree_segments(tree)
+    log_likelihood = 0
+    for (start, end) in segments:
+        log_likelihood += segment_log_likelihood(tree, start, end)
+    return log_likelihood
+
+if __name__ == "__main__":
+    t = TimeTree("((A:1, B:1):2, C:3);")
+    print(tree_log_likelihood(t))
+    t.show()
+
+

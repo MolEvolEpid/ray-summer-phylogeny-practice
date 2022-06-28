f85c7053825c62249d9d0a1d55be3fbefc12f45c
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-06-28 08:40:13 -0600

    Fix constant treegen code so it conforms to expected PDF function

--- tree_generation.py
+++ tree_generation.py
@@ -1,41 +1,27 @@
 #!/usr/bin/env python3
 
 import numpy as np
-import random
 from ete3 import TreeNode
-from time_tree import TimeTree
 from numpy.random import Generator, PCG64
 
-def generate_nodes(k):
-    return [TreeNode(dist=0, name=str(i)) for i in range(k)]
-
-def con_coalescence(nodes, params):
-    # TODO when we have a way to pick from exp and lin dists I can expand this
-
+def con_coalescence(nodes, params): # TODO expand with linear and exponential later
     # Time until coalescence occurs
-    k = len(nodes)
-    scale = (2*params["N"]) / (k*(k-1))
+    scale = (2*params["N"]) / (params["k"]*(params["k"]-1))
     rng = Generator(PCG64())
-    coal_time = rng.exponential(scale=scale) # TODO this is the big difference. is my scale right?
+    coal_time = rng.exponential(scale=scale)
+
+    # Add distance to all existing nodes to make sure they align
+    for n in nodes:
+        n.dist += coal_time
 
     # Choose nodes to pair
-    random.shuffle(nodes)
+    rng.shuffle(nodes)
     n1 = nodes.pop()
     n2 = nodes.pop()
 
-    # Assign each node a time so they will be even with each other
-    max_len = 0
-    for n in [n1, n2]:
-        n.len = n.get_farthest_node()[1]
-        if n.len > max_len:
-            max_len = n.len
+    # Give them a common parent
     parent = TreeNode(dist=0)
-    print(f"\ncoa {coal_time}")
-    print(f"max {max_len}")
     for n in [n1, n2]:
-        print(f"len {n.len}")
-        n.dist = coal_time + max_len - n.len
-        print(f"dst {n.dist}")
         parent.add_child(n)
     nodes.append(parent)
 
@@ -49,30 +35,11 @@ def generate_tree(params):
       k
       Any parameters required by probability (generally either N or N0 and another parameter)
     """
-    nodes = generate_nodes(params["k"])
+    nodes = [TreeNode(dist=0, name=str(i)) for i in range(params["k"])]
     while len(nodes) > 1:
         nodes = con_coalescence(nodes, params)
     return nodes[0].write(format=1)
 
-from tree_likelihood import tree_segments
-from plot_coalescence_time import probability_overlay, side_by_side
-from population_models import con_probability
-
-def plot_time(params):
-    times = []
-    for i in range(1000):
-        t = TimeTree(generate_tree(params))
-        groups = tree_segments(t)
-        times.append(groups[0][1]) # time until first coalescence on the tree
-    x = np.linspace(0, max(times), 1000)
-    y = [con_probability(params, z) for z in x]
-
-    labels = {"type": "Constant", "N": str(params["N"]), "k": str(params["k"])}
-
-    probability_overlay(times, x, y, labels)
-
 if __name__ == "__main__":
     params = {"N": 1000, "k": 20}
-    plot_time(params)
-    #t = TimeTree(generate_tree({"N": 1000, "k": 20}))
-    #t.write(format=1, outfile="out.nwk")
+    t = generate_tree(params)

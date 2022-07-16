96e5cf6c072bb13599c7893c71253c979f55b6c5
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-15 23:01:33 -0600

    Remove path dependenct from compile.sh

--- beamer/compile.sh
+++ beamer/compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-pdflatex /home/rheil/Code/ray-summer-phylogeny-practice/beamer/slides.tex
-evince /home/rheil/Code/ray-summer-phylogeny-practice/beamer/slides.pdf
+pdflatex slides.tex 
+evince slides.pdf 

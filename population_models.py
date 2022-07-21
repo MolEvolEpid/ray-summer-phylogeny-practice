48710df0ae1907908e21a9679b9d4c71fcc602da
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-21 16:38:29 -0600

    Rewrite population models to take different parameters
    I haven't updated any of the stuff that relies on that yet, and I doubt
    I'll be able to finish today. But it's coming along...I think

--- population_models.py
+++ population_models.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
-import math
-import random
-import numpy as np
-from plot_coalescence_time import probability_overlay, side_by_side
+from math import exp
 
-#
-# Effective population size over time for constant, linear, and exponential models
-#
+def check_params_exist(params, expected_params):
+    """
+    Raise an exception if any of the expected_params are not in params.
 
-def con_population(params, T):
+    Parameters:
+      params (dict): Dictionary containing any named parameters.
+      expected (list): List containing the keys of each expected param.
+
+    Returns:
+      Nothing, but may throw an Exception.
+    """
+    if any(map(lambda s: s not in params, expected_params)):
+        raise Exception(f"Please check params. Expected {expected_params}, given {list(params.keys())}")
+
+def con_population(params, t):
     """
     Return a constant population. Params and T are not necessary, they just exist so it is easier
     to swap constant population with linear and exponential.
@@ -18,16 +25,15 @@ def con_population(params, T):
       params (dict): Description of population size. Kept in a dictionary
       to match linear and exponential inputs.
         N (float): Population size
-      T (float): Time since infection
+      t (float): Time since infection
 
     Returns:
       population (float): Effective population size at specified time
     """
-    if "N" not in params:
-        raise Exception(f"Check params, given {params}")
+    check_params_exist(params, ['N'])
     return params["N"]
 
-def lin_population(params, t): # {"a": 1, "b": 3, "Dinf": 200} new param vector, Dinf is provided additionally (not from tree) 
+def lin_population(params, t):
     """
     Return the effective population T time units after infection
     using a linear model.
@@ -36,23 +42,21 @@ def lin_population(params, t): # {"a": 1, "b": 3, "Dinf": 200} new param vector,
       params (dict): Parameters describing the population dynamics
         a (float): Population at the time of infection
         b (float): Linear rate of population change (per generation)
-      t (float): 
+        I (float): Time of infection
+      t (float): Time since infection
 
     Returns:
       population (float): Effective population size at specified time
     """
-    if "a" not in params:
-        raise Exception(f"Check params, given {params}")
+    check_params_exist(params, ['a', 'b', 'I'])
     a = params["a"]
     b = params["b"]
-    inf_time = params["inf_time"]
-    return a + (inf_time-t)*b
+    I = params["I"]
+    return a + (I-t)*b
 
-#
-# Probability of a coalescence event occuring for each population model
-#
 
-def con_probability(params, z):
+# TODO Come up with a better way to talk about the `start` value. Maybe rename it too.
+def con_probability(params, start, z):
     """
     The proabaility of a coalescence at time z with constant population
 
@@ -60,34 +64,40 @@ def con_probability(params, z):
       params (dict): Parameters specifying the state of the tree at a certain point in time.
         k (int): Number of sequences
         N (float): Population size
-      z (float): Time until the coalescence event (from the current point in the tree)
+      start (float): Position of the start of the current tree segment
+      z (float): Time until the coalescence event (from start)
 
     Returns:
       probability (float): Probability of a coalescence event happening
       exactly at the specified time
     """
+    check_params_exist(params, ['k', 'N'])
     k = params["k"]
     N = params["N"]
     lmd = k*(k - 1)/(2*N)
-    return lmd * math.exp(-lmd*z)
+    return lmd * np.exp(-lmd*z)
 
-def lin_probability(params, now, z):
+def lin_probability(params, start, z):
     """
-    The proabaility of a coalescence at time z with constant population
+    The proabaility of a coalescence at time z with linear population
 
     Parameters:
       params (dict): Parameters specifying the state of the tree at a certain point in time.
         k (int): Number of sequences
         a (float): Population at time of infection
         b (float): Linear rate of effective population increase (per generation)
-        inf_time (float): TODO WHAT IS THIS
-      z (float): Time until the coalescence event (forward from T)
+        I (float): Time of infection
+      start (float): Position of the start of the current tree segment 
+      z (float): Time until the coalescence event (from start)
 
     Returns:
       probability (float): Probability of a coalescence event happening
       exactly at the specified time
     """
+    check_params_exist(params, ['k', 'a', 'b', 'I'])
     k = params["k"]
+    a = params["a"]
     b = params["b"]
-    return (k*(k-1)/(2*(pop_T - b*z))) * (pop_T / (pop_T - b*z))**(-k*(k-1)/(2*b))
+    I = params["I"]
+    return (k*(k-1) / 2) * (1 / (a+(b*(I-start-z)))) * ((a+(b*(I-start))) / (a+(b*(I-start-z)))) ** (-k*(k-1)/(2*b))
 

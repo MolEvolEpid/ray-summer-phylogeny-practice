52e33776c8108584ef0d775c0097d3648f91482e
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-22 09:30:32 -0600

    Make rescale script more flexible
    It now can take multiple files and files where the first line is a tree,
    but the rest are arbitrary (such as those having to do with host info)

--- rescale_trees.py
+++ rescale_trees.py
@@ -1,31 +1,18 @@
 from ete3 import Tree
 import sys
 
-def input_with_default(text, default):
-    """
-    Ask a question until either a float value is provided or
-    a blank field is entered. Return either the entered float 
-    or the specified default.
+######################################
+### Parameters that need to be set ###
+######################################
 
-    Parameters:
-      text (str): Prompt for the user to answer
-      default (float): Default value if nothing is entered.
-        Will be appended to the prompt in brackets.
+# The number of days in each unit of time on the input trees (default 365)
+time_unit_days = 365
 
-    Returns:
-      val (float): Either the user-specified number or the default value
-    """
-    question = text + " [" + str(default) + "]: "
-    response = input(question)
-    val = default
-    if response == '':
-        return val
-    else:
-        try:
-            val = float(response)
-        except ValueError:
-            input_with_default(text, default)
-    return val
+# The number of days a generation takes (default 1.5)
+generation_days = 1.5
+
+
+###############################################################################
 
 def scale_trees(tree_list, scale):
     """
@@ -35,21 +22,21 @@ def scale_trees(tree_list, scale):
     Parameters:
       tree_list (list): List containing one or more ete3 Tree objects
       scale (float): Scaling factor to change trees by
-      
+
     Returns:
       scaled_trees (list): New list containing the scaled Tree objects.
     """
-    # We don't wanna change the original list unexpectedly
+    # We don't want to change the original list unexpectedly
     scaled_trees = tree_list.copy()
     for tree in scaled_trees:
         for node in tree.traverse():
-            node.dist *= scale # TODO does this just work?
+            node.dist *= scale
     return scaled_trees
 
-def output_to_file(tree_list, outfile):
+def output_to_file(lines_out, outfile):
     """
     Write the Newick string representation of each tree in
-    the list to a file. 
+    the list to a file.
 
     ANY DATA IN THE FILE WILL BE OVERWRITTEN
 
@@ -60,51 +47,64 @@ def output_to_file(tree_list, outfile):
     Returns:
       nothing
     """
-    lines_out = [tree.write(format=1) + '\n' for tree in tree_list]
     with open(outfile, 'w') as treefile:
         treefile.writelines(lines_out)
     print(f"Successfully written to {outfile}.")
 
+def parse_trees(lines):
+    """
+    Provided a list of lines from any file, determine
+    if any trees are in the file and scale them.
+
+    Parameters:
+      lines (list): List of line strings, read from a file
+
+    Returns:
+      lines (list): List of line strings, but with trees scaled.
+    """
+    try:
+        trees = [Tree(l) for l in lines]
+        rest = []
+    except Exception as e:
+        if type(e).__name__ == "NewickError":
+            trees = [Tree(lines[0])]
+            rest = lines[1:]
+        else:
+            raise Exception(e)
+    scale = time_unit_days / generation_days
+    trees_scaled = scale_trees(trees, scale)
+    tree_strings = [t.write(format=1) + '\n' for t in trees_scaled]
+    return tree_strings + rest
+
 def main():
     """
-    Ask the user about the scaling of the existing tree(s) in a file
-    and make the necessary calls to transform it to generations.
+    For each file provided as a command line argument, attempt to process it
+    and scale it up as specified at the top of the file.
     """
     # Check that a file exists and we can try to read it
+    if sys.argv[1:] == []:
+        print('No input file name was provided. Please provide the relative path\nto a file containing one or more trees in Newick format.')
+        return 1
     try:
-        with open(sys.argv[1]) as treefile:
-            nwk_list = treefile.readlines()
-    except IndexError:
-        print('No input file name was provided. Please provide the relative path\nof a file containing one or more trees in Newick format.')
-        return 1
-    except FileNotFoundError:
-        print('An input was provided, but no file was found with that name.\nPlease check that you have spelled the name correctly and that the file is readable.')
-        return 1
-
-    # Try to read the file and make it into trees
-    tree_list = []
-    for nwk in nwk_list:
-        try:
-            tree_list.append(Tree(nwk))
-        except Exception as e:
-            if type(e).__name__ == 'NewickError':
-                print(f'The file cannot be converted into a tree. Expected newick formatted text, got \"{nwk[:-1]}\"')
-                return 1
-            else:
-                raise Exception(e)
-
-    # Ask for scaling factors about the input tree
-    days_per_timeunit = input_with_default('On the input tree(s), how many days are there per time unit?', 365.0)
-    days_per_generation = input_with_default('How many days are there in a generation?', 1.5)
-
-    # Scale each tree in the file
-    scale = days_per_timeunit / days_per_generation
-    scaled_trees = scale_trees(tree_list, scale)
-
-    # Make a new file name and output to that file
-    name, extension = sys.argv[1].split('.') # TODO I could handle this better but it may be okay
-    outname = name + "_scaled." + extension
-    output_to_file(scaled_trees, outname)
+        for arg in sys.argv[1:]:
+            print(f'Processing {arg}...')
+            with open(arg) as f:
+                lines = f.readlines()
+                first_tree = Tree(lines[0]) # Attempt to convert the first line, catch the error it may throw
+                lines_out = parse_trees(lines)
+                name, ext = arg.split('.')
+                name_out = name + "_rescaled." + ext
+                output_to_file(lines_out, name_out)
+        return 0
+    except Exception as e:
+        if type(e).__name__ == "FileNotFoundError":
+            print('An input was provided, but no file was found with that name.\nPlease check that you have spelled the name correctly and that the file is readable.')
+            return 1
+        elif type(e).__name__ == "NewickError":
+            print('The file does not seem to contain tree data. Please ensure\nthat the first line in the file is a tree.')
+            return 1
+        else:
+            raise Exception(e)
 
 if __name__ == '__main__':
     main()

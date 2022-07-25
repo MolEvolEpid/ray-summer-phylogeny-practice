52a33547bc508e96c3c6f8c924814e1d7dfb8e7a
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-25 15:11:29 -0600

    Update issues script with latest problem

--- current_issues.py
+++ current_issues.py
@@ -5,32 +5,42 @@ from new_optimization import *
 
 # Create a few trees from files
 # All trees were generated with Erik's simulator and scaled by my script
+with open("eric-sim/b3/tree01_rescaled.txt") as f:
+    t_b3_normal = TimeTree(f.readline())
 with open("eric-sim/b3/tree02_rescaled.txt") as f:
-    t_b3 = TimeTree(f.readline())
-with open("eric-sim/b5/tree01_rescaled.txt") as f:
-    t_b5 = TimeTree(f.readline())
-with open("eric-sim/b10/tree01_rescaled.txt") as f:
-    t_b10 = TimeTree(f.readline())
+    t_b3_broken = TimeTree(f.readline())
 
-# Range to search a and b
-a_min, a_max = 0, 5
-b_min, b_max = 1, 5
-a_range = np.linspace(a_min, a_max, 100)
-b_range = np.linspace(b_min, b_max, 100)
+# Gridsearch
+# For some trees, the gridsearch reveals a hard edge to the valid values.
+# This is not present for other trees.
+def plot_gridsearch(t, a_bounds, b_bounds):
+    a_min, a_max = a_bounds
+    b_min, b_max = b_bounds
+    a_range = np.linspace(a_min, a_max, 100)
+    b_range = np.linspace(b_min, b_max, 100)
 
-# Plot likelihood for each of those points
-# I can't get the axes to show the right values but that's neither here nor there
-fig, ax = plt.subplots()
+    values = simple_gridsearch(t, a_range, b_range, 2*(365/1.5))
 
-values = simple_gridsearch(t_b3, a_range, b_range, 2 * (365/1.5))
+    fig, ax = plt.subplots()
+    cax = ax.imshow(values, origin="lower")
+    cbar = fig.colorbar(cax)
 
-cax = ax.imshow(values, origin="lower")
-ax.set_xlabel("Values of beta")
-ax.set_ylabel("Values of alpha")
-ax.set_xticks((0, 100))
-ax.set_yticks((0, 100))
-ax.set_xticklabels((b_min, b_max))
-ax.set_yticklabels((a_min, a_max))
-cbar = fig.colorbar(cax)
+    ax.set_xlabel("Beta")
+    ax.set_ylabel("Alpha")
+    ax.set_xticks((0, 100))
+    ax.set_yticks((0, 100))
+    ax.set_xticklabels((b_min, b_max))
+    ax.set_yticklabels((a_min, a_max))
+    plt.show()
 
-plt.show()
+plot_gridsearch(t_b3_normal, (1, 200), (1, 200))
+plot_gridsearch(t_b3_broken, (1, 200), (1, 200))
+
+# Optimization
+# With the trees with
+t1 = optimize_a_b(t_b3_normal, (1, 1), 2*(365/1.5))
+t2 = optimize_a_b(t_b3_normal, (2, 2), 2*(365/1.5))
+t3 = optimize_a_b(t_b3_normal, (1, 3), 2*(365/1.5))
+t4 = optimize_a_b(t_b3_normal, (3, 1), 2*(365/1.5))
+t5 = optimize_a_b(t_b3_normal, (5, 5), 2*(365/1.5))
+t6 = optimize_a_b(t_b3_normal, (5, 3), 2*(365/1.5))
--- new_optimization.py
+++ new_optimization.py
@@ -2,68 +2,24 @@ from scipy.optimize import minimize
 from tree_likelihood import tree_likelihood
 from time_tree import TimeTree
 from population_models import *
-import random
+import numpy as np
 
-def optimize_linear(tree, x0):
-    fun = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"N0": N0, "b": b})
-    res = minimize(fun, x0, method="Nelder-Mead")
+def optimize_b(tree, a, b0, I):
+    fun = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"a": a, "b": b0, "I": I})
+    res = minimize(fun, b0, method="Nelder-Mead")
     return res
 
-def optimize_worse(tree, N0):
-    fm = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"N0": x, "b": 1095})
-    res = minimize(fm, N0, method="BFGS")
+def optimize_a_b(tree, p0, I):
+    fun = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"a": x[0], "b": x[1], "I": I})
+    res = minimize(fun, p0, method="Nelder-Mead")
     return res
 
-def different_cold_starts(tree, starts):
-    for x0 in starts:
-        res = optimize_linear(tree, x0)
-        print(x0, res.x, res.success)
-    
-def random_startpoint_list(n, low, high):
-    return [(random.randint(low, high), random.randint(low, high)) for _ in range(n)]
-
-def simple_gridsearch(tree, N0_range, b_range):
-    values = np.zeros((len(N0_range), len(b_range)))
+def simple_gridsearch(tree, a_range, b_range, I):
+    values = np.zeros((len(a_range), len(b_range)))
     # b along rows, N0 down columns
     for i, b in enumerate(b_range):
-        for j, N0 in enumerate(N0_range):
-            val = tree_likelihood(tree, lin_population, lin_probability, {"N0": N0, "b": b})
+        for j, N0 in enumerate(a_range):
+            val = tree_likelihood(tree, lin_population, lin_probability, {"a": N0, "b": b, "I": I})
             values[j][i] = val
     return values
-
-import matplotlib.pyplot as plt
-
-if __name__ == "__main__":
-    treefile = open("linear-latest.tre")
-    t = TimeTree(treefile.readline())
-    treefile.close()
-
-    #starts = [(1, 1), (300, 300), (1000, 1000), (100, 1), (1, 100), (1100, 1100)]
-    #starts = random_startpoint_list(100, 1100, 2500)
-    #different_cold_starts(t, starts)
-
-    min_N0, max_N0 = (1, 5)
-    min_b, max_b = (1, 5)
-    N0_range = np.linspace(min_N0, max_N0, 100)
-    b_range = np.linspace(min_b, max_b, 100)
-
-    values = simple_gridsearch(t, N0_range, b_range)
-    print(values)
-
-    plt.imshow(values, origin="lower") # Flip vertically to make (0, 0) in bottom left corner
-    plt.xlabel("Values of N0")
-    plt.ylabel("Values of b")
-    plt.colorbar()
-
-    """
-    # It probably isn't actually this hard to make the axes line up
-    num_x_ticks = len(ax.get_xticklabels())
-    num_y_ticks = len(ax.get_yticklabels())
-    x_ticks = np.arange(min_N0, max_N0+min_N0, (max_N0 - min_N0) / num_x_ticks)
-    y_ticks = np.arange(min_b, max_b+min_b, (max_b - min_b) / num_y_ticks)
-    ax.set_xticklabels(x_ticks)
-    ax.set_yticklabels(y_ticks)
-    """
-
-    plt.show()
 

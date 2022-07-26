1f70672eb975e71819775affb9e79b0ca74e0d61
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-26 09:51:43 -0600

    Better parameter checking in tree likelihood
    Also updated current issue file to better bring out what's going on

--- current_issues.py
+++ current_issues.py
@@ -5,15 +5,15 @@ from new_optimization import *
 
 # Create a few trees from files
 # All trees were generated with Erik's simulator and scaled by my script
-with open("eric-sim/b3/tree01_rescaled.txt") as f:
+with open("eric-sim/b5/tree02_rescaled.txt") as f:
     t_b3_normal = TimeTree(f.readline())
-with open("eric-sim/b3/tree02_rescaled.txt") as f:
+with open("eric-sim/b5/tree01_rescaled.txt") as f:
     t_b3_broken = TimeTree(f.readline())
 
 # Gridsearch
 # For some trees, the gridsearch reveals a hard edge to the valid values.
 # This is not present for other trees.
-def plot_gridsearch(t, a_bounds, b_bounds):
+def plot_gridsearch(t, a_bounds, b_bounds, title):
     a_min, a_max = a_bounds
     b_min, b_max = b_bounds
     a_range = np.linspace(a_min, a_max, 100)
@@ -25,22 +25,18 @@ def plot_gridsearch(t, a_bounds, b_bounds):
     cax = ax.imshow(values, origin="lower")
     cbar = fig.colorbar(cax)
 
+    ax.set_title(title)
     ax.set_xlabel("Beta")
     ax.set_ylabel("Alpha")
-    ax.set_xticks((0, 100))
-    ax.set_yticks((0, 100))
-    ax.set_xticklabels((b_min, b_max))
-    ax.set_yticklabels((a_min, a_max))
+    ax.set_xticks((0, 20, 40, 60, 80, 100)) # Set to known values so it's easy to relabel
+    ax.set_yticks((0, 20, 40, 60, 80, 100))
+    ax.set_xticklabels(np.round(np.linspace(b_min, b_max, 6), 1))
+    ax.set_yticklabels(np.round(np.linspace(a_min, a_max, 6), 1))
     plt.show()
 
-plot_gridsearch(t_b3_normal, (1, 200), (1, 200))
-plot_gridsearch(t_b3_broken, (1, 200), (1, 200))
+#plot_gridsearch(t_b3_normal, (0, 10), (1, 10), "Expected behavior")
 
-# Optimization
-# With the trees with
-t1 = optimize_a_b(t_b3_normal, (1, 1), 2*(365/1.5))
-t2 = optimize_a_b(t_b3_normal, (2, 2), 2*(365/1.5))
-t3 = optimize_a_b(t_b3_normal, (1, 3), 2*(365/1.5))
-t4 = optimize_a_b(t_b3_normal, (3, 1), 2*(365/1.5))
-t5 = optimize_a_b(t_b3_normal, (5, 5), 2*(365/1.5))
-t6 = optimize_a_b(t_b3_normal, (5, 3), 2*(365/1.5))
+# I think the warning I implemented that the end of the tree passes
+# over the infection time helps solve this issue.
+plot_gridsearch(t_b3_broken, (0, 10), (1, 10), "Confusing behavior")
+
--- tree_likelihood.py
+++ tree_likelihood.py
@@ -28,27 +28,38 @@ def tree_likelihood(tree, population, probability, params):
     Return the log likelihood of a tree existing based
     on the given population model and probability function.
     """
+    # Make sure the tree starts at or after I. If not, warn the user.
+    if params['I'] < tree.time:
+        warnings.warn(f"Tree time {tree.time} was further back than transmission time {params['I']}")
+
+    # Check params are valid, return -inf if they are not.
+    # Does not raise warnings, as it is meant to work with an optimizer.
+    if 'b' in params: # Linear model
+        if params['b'] <= 0:
+            return -np.inf
+        if params['a'] < 0:
+            return -np.inf
+    elif 'N' in params: # Constant model
+        if params['N'] <= 0:
+            return -np.inf
+    else: # Neither model fits -> raise an error
+        raise Exception("params contained neither b nor N, so a population model could not be determined.")
+
+    # If we've not had any issues yet, find the probability step by step
     params_now = params.copy()
-
     log_likelihood = 0
-    params_now["k"] = len(tree.get_leaves())
-
-    if params_now['b'] < 0:
-        return -np.inf
-    elif params_now['a'] < 0:
-        return -np.inf
-    # we will also eventually want a bound on I, but it doesn't make sense to have one now.
+    params_now["k"] = len(tree.get_leaves()) # TODO this will not work when we have multiple hosts
 
     for (start, end, dist) in tree_segments(tree):
-        if params_now["k"] == 1:
-            warnings.warn(f"k was 1 between {start} and {start+dist}. The code can't handle this yet.")
+        segment_lk = np.log(probability(params_now, start, dist))
+        if params_now["k"] == 1: # TODO is this check still necessary?
+            warnings.warn(f"Only one node from {start} to {end}. Not sure what's happening.")
         if end > params_now["I"]:
-            warnings.warn(f"The end of the tree passes over the infection time, which the code can't handle yet.")
-        else:
-            segment_lk = np.log(probability(params_now, start, dist))
-            if np.isnan(segment_lk):
-                warnings.warn("segment likelihood came out as nan. You found a case that bypasses the existing bounds.")
-            log_likelihood += segment_lk
+            warnings.warn(f"Tree node at {end} was beyond I {params_now['I']}. We should have 2 hosts but don't.")
+        if np.isnan(segment_lk):
+            warnings.warn(f"Segment likelihood was nan from {start} to {end}. This didn't get caught already.") # TODO should no longer be necessary
+        log_likelihood += segment_lk
         params_now["k"] -= 1
+
     return log_likelihood
 

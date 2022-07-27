37e17b5c80b8485d7e17394407cfd660dce798b4
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-27 16:47:56 -0600

    Initial work on tree segments with multiple hosts
    Haven't been able to test. Also, it's still quite messy and could use
    reoganization.

--- tree_likelihood.py
+++ tree_likelihood.py
@@ -23,6 +23,65 @@ def tree_segments(tree):
         segments.append((start, end, end-start))
     return segments
 
+def tree_segments_multihost(tree, T):
+    """
+    Divide a tree into three types of segments: Those that occur entirely in either 
+    the donor or the recipient and those that occur across the transmission time.
+
+    Parameters:
+      tree (TimeTree): ete3 representation of a tree with time values.
+      T (float): Time of transmission between donor and recipient. 
+        Also can be written As I_R (I sub R)
+
+    Returns:
+      donor_segments (list): All segments which happen in the donor, 
+        including those between T and the root of the tree.
+      recip_segments (list): All segments which occur entirely in the
+        recipient, not counting those that cross over T.
+      tmiss_segments (list): All segments which cross over the transmission
+        time T, from the recipient to the donor.
+    """
+    # TODO For this, I rely on having each leaf have a host attribute that is 0 or 1.
+    # I have a way to calculate these, but I was fighting with the class to get it
+    # to calculate that upon creation of the object.
+
+    t.populate_hosts({"D": 0, "R": 1}) # Overwrite any existing host data for what we'll use
+
+    for node in tree.iter_descendants():
+        coal_D = [] # Intervals where coalescences occur normally
+        coal_R = []
+        none_D = [] # Intervals where no coalescence occurs
+        none_R = []
+
+        # The start and end of the branch from this node back
+        branch_start = node.up.time
+        branch_end = node.time
+
+        # T is in the middle of the branch
+        # We need to spli everything up. But how? 
+        if branch_start >= T > branch_end:
+            if node_host == 0:
+                none = none_D
+            elif node_host == 1:
+                none = none_R
+            else:
+                raise Exception("Host was neither 0 or 1. This should not have happened.")
+            none.append((branch_start, T, T-branch_start))
+            coal_D.append((T, branch_end, branch_end-T)) # Other side is always in D
+        # Chronologically after T (towards tips)
+        # The children will all be of the same host, so we only need to check one
+        elif branch_start < T and branch_end < T:
+            if node_host == 0:
+                coal_D.append((branch_start, branch_end, branch_end-branch_start))
+            elif node_host == 1:
+                coal_R.append((branch_start, branch_end, branch_end-branch_start))
+            else:
+                raise Exception("Host was neither 0 or 1. This should not have happened.")
+        # Chronologically before T (towards root)
+        # These branches always have to be contained in the donor.
+        elif branch_start >= T and branch_end >= T:
+            coal_R.append((branch_start, branch_end, branch_end-branch_start))
+
 def tree_likelihood(tree, population, probability, params):
     """
     Return the log likelihood of a tree existing based

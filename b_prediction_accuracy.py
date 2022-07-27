557674cb647d1b2102716f8a5d7f60138cb5f46d
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-27 11:09:19 -0600

    Different data in erik-sim
    Now there are fewer options, but still hopefully enough to have a
    meaningful plot at the end. Writing to a .csv seems promising.

--- b_prediction_accuracy.py
+++ b_prediction_accuracy.py
@@ -1,54 +1,52 @@
 import numpy as np
 import os
+import csv
 import matplotlib.pyplot as plt
 from population_models import *
 from tree_likelihood import *
-from basic_optimization import *
+from new_optimization import *
 from time_tree import TimeTree
 
-def read_erik_sim_dir(): # TODO I broke this whole thing didn't I lol love it
+def calculate_all_sim_trees():
     """
-    Read the 'trees.tre' file in every subdirectory of erik-sim.
-
-    Very hardcoded. Be careful that you run from the same directory
-    as this file.
+    For every tree in erik-sim, calculate the MLE and compare it to the actual value.
+    
+    Write these results to a csv file.
     """
-    all_data = {}
-    for dirname in os.listdir("erik-sim"):
-        print(f"Now reading {dirname}")
-        treefile = ("erik-sim/" + dirname + "/trees.tre") # bootleg but it'll do
-        with open(treefile) as f:
-            trees = [TimeTree(l) for l in f.readlines()]
-            all_data[dirname] = [t for t in trees if t.time <= 2*(365/1.5)] # Also hard coded. Filter out "invalid" trees.
-    return all_data
+    with open("erik-sim/summary.csv", "w") as outfile:
+        # Initialize CSV file overwriting any previous data
+        fieldnames = ['file', 'line', 'real_a', 'real_k', 'real_b', 'mle_b', 'success']
+        writer = csv.DictWriter(outfile, delimiter=',', fieldnames=fieldnames)
+        writer.writeheader()
 
-def organize_data(data):
-    organized_a = {}
-    organized_k = {}
-    for key in data:
-        a = int(key[1:2])
-        k = int(key[4:6])
-        b = int(key[8:])
-        
-        if a in organized_a:
-            if b in organized_a[a]:
-                organized_a[a][b] += data[key]
-            else:
-                organized_a[a][b] = data[key]
-        else:
-            organized_a[a] = {}
-            organized_a[a][b] = data[key]
-        
-        if k in organized_k:
-            if b in organized_k[k]:
-                organized_k[k][b] += data[key]
-            else:
-                organized_k[k][b] = data[key]
-        else:
-            organized_k[k] = {}
-            organized_k[k][b] = data[key]
+        # Read trees within each directory
+        to_read = sorted([s for s in os.listdir("erik-sim") if s[-4:] != ".csv"]) # Do not read csv files. Very bootleg.
+        for dirname in to_read: 
+            real_a, real_k, real_b = [float(term[1:]) for term in dirname.split("_")] # "a5_k100_b4" -> [5.0, 100.0, 4.0]
+            treefile = ("erik-sim/" + dirname + "/trees.tre")
+            with open(treefile) as f:
+                print(f"Calculating trees from {treefile}...")
+                # Filter trees by those that do not have invalid root times.
+                # Will introduce bias in the data, especially for higher b.
+                all_data = [TimeTree(l) for l in f.readlines()]
+                valid_lines, valid_data = [], []
+                for i, t in enumerate(all_data):
+                    if t.time <= 2*(365/1.5): # Hardcoded time of I
+                        valid_lines.append(i)
+                        valid_data.append(t)
+                valid_lines = valid_lines[:100]
+                valid_data = valid_data[:100]
 
-    return organized_a, organized_k
+                # For each valid tree, record it into the data file.
+                for line, tree in zip(valid_lines, valid_data):
+                    res = optimize_b(tree, real_a, 2*(365/1.5))
+                    writer.writerow({'file': treefile,
+                                     'line': line+1,
+                                     'real_a': real_a, 
+                                     'real_k': real_k, 
+                                     'real_b': real_b, 
+                                     'mle_b': res.x,
+                                     'success': res.success})
 
 def b_prediction_accuracy(org_a, org_k):
     """
@@ -61,14 +59,12 @@ def b_prediction_accuracy(org_a, org_k):
     k_fig, ((k_ax1, k_ax2), (k_ax3, k_ax4)) = plt.subplots(2, 2)
 
     for a in org_a:
-        for b in a:
+        for b in org_a[a]:
             pass # THIS ISN'T DONE YET, SORRY FOR ANY CONFUSION
 
 
 def main():
-    all_data = read_erik_sim_dir()
-    org_a, org_k = organize_data(all_data)
-    b_prediction_accuracy(org_a, org_k)
+    calculate_all_sim_trees()
 
 if __name__ == '__main__':
     main()

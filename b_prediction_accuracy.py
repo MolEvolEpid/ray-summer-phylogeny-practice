9b82d62de7a71f78704664aa1cb687046ed33151
Raymond Heil <68617358+raymondheil@users.noreply.github.com>
2022-07-27 14:42:32 -0600

    Plot prediction accuracy of b
    In its own file because I wrote so many helper I/O functions for it.
    Maybe they can be consolidated into presentation_plot soon if I end up
    using them.

--- b_prediction_accuracy.py
+++ b_prediction_accuracy.py
@@ -2,10 +2,10 @@ import numpy as np
 import os
 import csv
 import matplotlib.pyplot as plt
-from population_models import *
-from tree_likelihood import *
 from new_optimization import *
+from basic_optimization import error_hdi
 from time_tree import TimeTree
+from arviz import hdi
 
 def calculate_all_sim_trees():
     """
@@ -48,23 +48,74 @@ def calculate_all_sim_trees():
                                      'mle_b': res.x,
                                      'success': res.success})
 
-def b_prediction_accuracy(org_a, org_k):
-    """
-    First, plot the b predictions if we fix k and vary alpha.
+def plot_from_summary_csv():
+    # Read the summary csv file into two dicts to be plotted
+    changing_k = {}
+    changing_a = {}
+    with open("erik-sim/summary.csv", "r") as infile:
+        reader = csv.DictReader(infile)
+        for row in reader:
+            # Typecast the relevant columns to be easier to work with
+            for col in ['line', 'real_a', 'real_k', 'real_b', 'mle_b']:
+                row[col] = float(row[col])
+            row["success"] = bool(row["success"])
 
-    Then, plot the b predictions if we fix alpha and vary k.
-    """
-    # Initialize figures
-    a_fig, ((a_ax1, a_ax2), (a_ax3, a_ax4)) = plt.subplots(2, 2)
-    k_fig, ((k_ax1, k_ax2), (k_ax3, k_ax4)) = plt.subplots(2, 2)
+            # Convenience variables for the most important cols
+            a = row["real_a"]
+            k = row["real_k"]
+            b = row["real_b"]
+            mle_b = row["mle_b"]
 
-    for a in org_a:
-        for b in org_a[a]:
-            pass # THIS ISN'T DONE YET, SORRY FOR ANY CONFUSION
+            # Read the data into dictionaries. 
+            # These won't work with more complex data, I'll need something more SQL-like
+            for key, d in zip([a, k], [changing_a, changing_k]):
+                if key in d:
+                    if b in d[key]:
+                        d[key][b].append(mle_b)
+                    else:
+                        d[key][b] = [mle_b]
+                else:
+                    d[key] = {b: [mle_b]}
+
+    # Plot the data in those dicts
+    k_fig, (k_ax1, k_ax2) = plt.subplots(1, 2)
+    a_fig, (a_ax1, a_ax2) = plt.subplots(1, 2)
+
+    for val, ax, d in zip([1, 5, 20, 100],
+                          [a_ax1, a_ax2, k_ax1, k_ax2],
+                          [changing_a, changing_a, changing_k, changing_k]):
+        x = list(d[val].keys())
+        all_mle = [d[val][n] for n in x]
+        mean_mle = []
+        error = []
+        for points in all_mle:
+            mean_mle.append(sum(points) / len(points))
+            error.append(error_hdi(points))
+        error = np.transpose(error)
+        ax.errorbar(x, mean_mle, yerr=error, fmt='o')
+        ax.plot(x, x)
+
+        ax.set_yticks(x)
+        ax.set_xticks(x)
+        ax.set_xlabel("Simulated value of β")
+        ax.set_ylabel("Mean MLE of β")
+
+
+    k_ax1.set_title("α = 1, k = 20")
+    k_ax2.set_title("α = 5, k = 20")
+    a_ax1.set_title("α = 5, k = 20")
+    a_ax2.set_title("α = 5, k = 100")
+
+    k_fig.suptitle("β Prediction accuracy with different k")
+    a_fig.suptitle("β Prediction accuracy with different α")
+
+    plt.show()
+
 
 
 def main():
-    calculate_all_sim_trees()
+    #calculate_all_sim_trees() # To write data to the file. Only do this once, it won't change
+    plot_from_summary_csv()
 
 if __name__ == '__main__':
     main()
--- new_optimization.py
+++ new_optimization.py
@@ -1,17 +1,17 @@
-from scipy.optimize import minimize
+from scipy.optimize import minimize, minimize_scalar
 from tree_likelihood import tree_likelihood
 from time_tree import TimeTree
 from population_models import *
 import numpy as np
 
-def optimize_b(tree, a, b0, I):
-    fun = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"a": a, "b": b0, "I": I})
-    res = minimize(fun, b0, method="Nelder-Mead")
+def optimize_b(tree, a, I):
+    fun = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"a": a, "b": x, "I": I})
+    res = minimize_scalar(fun, method="brent") # TODO is brent any good at this or should I look back at BFGS and Nelder-Mead
     return res
 
-def optimize_a_b(tree, p0, I):
+def optimize_a_b(tree, x0, I): # Possibly less useful for now, it always wants a as low as possible
     fun = lambda x: -tree_likelihood(tree, lin_population, lin_probability, {"a": x[0], "b": x[1], "I": I})
-    res = minimize(fun, p0, method="Nelder-Mead")
+    res = minimize(fun, x0, method="Nelder-Mead")
     return res
 
 def simple_gridsearch(tree, a_range, b_range, I):
